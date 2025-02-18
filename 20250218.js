2d469dc75584aea172c4baf3bdaabee8c398a69c
Taka41802 <p280132a@gmail.com>
2025-02-18 15:17:17 +0900

    jsの学習

---
+++ 20250218.js
@@ -0,0 +1,15 @@
+sayHi = function (name) {
+  return "Hi ${name}!";
+};
+アロー関数;
+sayHi = (name) => {
+  return "Hi ${name}!";
+};
+
+3つの違い
+・一つの式をリターンする場合、{}とreturnを消せる
+  sayHi = (name) => "Hi ${name}!";
+・パラメーターが一つの場合()を省略することができる
+  sayHi = name => "Hi ${name}!";
+・ディスについて違う？後に説明
+
